d188a8af6217b6302d7913a06fa3e5a4c55af61b
shantohx <shantohx@gmail.com>
2021-08-07 03:59:02 +0600

    Issue #10 UI still doesn't work

--- Theory/Assignment/Assignment01/app/src/main/java/com/shanto/project1/SecondActivity.java
+++ Theory/Assignment/Assignment01/app/src/main/java/com/shanto/project1/SecondActivity.java
@@ -19,11 +19,11 @@ public class SecondActivity extends AppCompatActivity {
         String date0 = intent.getStringExtra(MainActivity.EXT2);
         String blood = intent.getStringExtra(MainActivity.EXT3);
         TextView textView = findViewById(R.id.text_message);
-//        TextView textView2 = findViewById(R.id.text_message2);
+        TextView textView2 = findViewById(R.id.text_message2);
 //        TextView textView3 = findViewById(R.id.text_message3);
 //        TextView textView4 = findViewById(R.id.text_message4);
-        textView.setText(name);
-//        textView2.setText(date0);
+        textView.setText(date0);
+        textView2.setText(date0);
 //        textView3.setText(NID);
 //        textView4.setText(blood);
     }
