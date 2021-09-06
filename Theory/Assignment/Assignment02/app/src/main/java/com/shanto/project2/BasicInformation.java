2757755a1b8789fc00e1e1721be21fb9104367c8
shantohx <shantohx@gmail.com>
2021-09-07 03:12:47 +0600

    #16 Renamed Project to Assignment 2
    Also progressed on Tab View

---
+++ Theory/Assignment/Assignment02/app/src/main/java/com/shanto/project2/BasicInformation.java
@@ -0,0 +1,29 @@
+package com.shanto.project2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BasicInformation#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BasicInformation extends Fragment implements {
+
+
+    public BasicInformation() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_basic_information, container, false);
+    }
+}
